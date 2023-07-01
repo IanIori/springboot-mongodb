99d9ae21fd906bfa08c24b8a62e01c81fadcca61
Ian Iori <ianmessia@gmail.com>
2023-07-01 16:35:40 -0300

    Getting an user by Id and Not Found exception

--- src/main/java/com/ianiori/springbootmongo/services/UserService.java
+++ src/main/java/com/ianiori/springbootmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.ianiori.springbootmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.ianiori.springbootmongo.domain.User;
 import com.ianiori.springbootmongo.repositories.UserRepository;
+import com.ianiori.springbootmongo.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,6 +18,11 @@ public class UserService {
 	
 	public List<User> findAll() {
 		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
 	}
 
 }
