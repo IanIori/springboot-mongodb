9066ecf91c0213ff91f0cb85f2acc5929b1e10d0
Ian Iori <ianmessia@gmail.com>
2023-07-02 13:10:59 -0300

    Referencing a user's posts

--- src/main/java/com/ianiori/springbootmongo/config/TestConfig.java
+++ src/main/java/com/ianiori/springbootmongo/config/TestConfig.java
@@ -42,6 +42,9 @@ public class TestConfig implements CommandLineRunner{
 		Post post2 = new Post(null, sdf.parse("23/03/2023"), "Good morning", "Woke up feeling good!", new AuthorDTO(maria));
 		
 		postRepository.saveAll(Arrays.asList(post1,post2));
+		
+		maria.getPosts().addAll(Arrays.asList(post1, post2));
+		userRepository.save(maria);
 	}
 
 
