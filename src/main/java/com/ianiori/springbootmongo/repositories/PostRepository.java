5462fa7c7a47389c2bd654a171ac9057f19515f2
Ian Iori <ianmessia@gmail.com>
2023-07-02 19:46:36 -0300

    Simple query method

--- src/main/java/com/ianiori/springbootmongo/repositories/PostRepository.java
+++ src/main/java/com/ianiori/springbootmongo/repositories/PostRepository.java
@@ -1,5 +1,7 @@
 package com.ianiori.springbootmongo.repositories;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,6 @@ import com.ianiori.springbootmongo.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
 
+	public List<Post> findByTitleContainingIgnoreCase(String text);
+	
 }
